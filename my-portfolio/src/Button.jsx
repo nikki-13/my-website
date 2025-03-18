a72608ae7e11183ef0f26b9bf6e9a4a648ffb46b
nikki-13 <somanikhilsai13@gmail.com>
2025-03-18 22:42:01 +0530

    abcd

--- my-portfolio/src/Button.jsx
+++
@@ -1,7 +0,0 @@
-export default function Button({ onClick, children }) {
-    return (
-      <button className="p-2 bg-blue-500 text-white rounded" onClick={onClick}>
-        {children}
-      </button>
-    );
-  }
